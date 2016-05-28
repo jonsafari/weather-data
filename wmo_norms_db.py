c66e09a35330db617d66c97753743de5676210c5
Jon Dehdari <jon@dehdari.org>
2016-05-28 21:04:22 +0200

    wmo_norms_db.py: use external wmo tables

--- wmo_norms_db.py
+++ wmo_norms_db.py
@@ -11,27 +11,49 @@ import os, sys, argparse, lzma, codecs
 
 parser = argparse.ArgumentParser(description='Builds weather database')
 
+parser.add_argument('--db', help='Specify Sqlite3 database output (default: %(default)s)', type=str, default='wmo_norms.db')
 parser.add_argument('--allnorms', help='Specify allnorms.dat input (default: %(default)s)', type=str, default='wmo_norms_1961-1990/data/allnorms.dat.utf8.xz')
 parser.add_argument('--stnmeta', help='Specify stnmeta.dat input (default: %(default)s)', type=str, default='wmo_norms_1961-1990/data/stnmeta.dat.utf8.xz')
-parser.add_argument('--db', help='Specify Sqlite3 database output (default: %(default)s)', type=str, default='wmo_norms.db')
+parser.add_argument('--climate_elem_codes', help='Specify climate_elem_code.tsv (default: %(default)s)', type=str, default='wmo_norms_1961-1990/data/climate_elem_code.tsv')
+parser.add_argument('--region_codes', help='Specify region_code.tsv (default: %(default)s)', type=str, default='wmo_norms_1961-1990/data/region_code.tsv')
+parser.add_argument('--stat_codes', help='Specify statistic_code.tsv (default: %(default)s)', type=str, default='wmo_norms_1961-1990/data/statistic_code.tsv')
 args = parser.parse_args()
 
+
+
+# Read climate element codes
+# Table 6 of wmo_norms/data/ALLNORMS.TXT
+# Code, Unit, Description
+clim_elem_codes = []
+with open(args.climate_elem_codes) as clim_elem_codes_file:
+    for line in clim_elem_codes_file:
+        clim_elem_codes.append(tuple(line.rstrip().split('\t')))
+
+# Read region codes
+# Table 1 of wmo_norms/data/ALLNORMS.TXT
+# Code, Region
+region_codes = []
+with open(args.region_codes) as region_codes_file:
+    for line in region_codes_file:
+        region_codes.append(tuple(line.rstrip().split('\t')))
+
+# Read statistic codes
+# Table 7 of wmo_norms/data/ALLNORMS.TXT
+# Code, Description
+statistic_codes = []
+with open(args.stat_codes) as statistic_codes_file:
+    for line in statistic_codes_file:
+        statistic_codes.append(tuple(line.rstrip().split('\t')))
+
+
+# Main weather data (allnorms)
+# File: wmo_norms/data/allnorms.dat.utf8.xz
+# Docs: wmo_norms/doc/allnorms.txt
 _, allnorms_suffix = os.path.splitext(args.allnorms)
 if allnorms_suffix == '.xz':
     allnorms_file = lzma.open(args.allnorms, mode='rt', encoding='utf-8')
 else:
     allnorms_file = codecs.open(args.allnorms, 'r', 'iso-8859-1') # original file is iso-8859
-
-_, stnmeta_suffix  = os.path.splitext(args.stnmeta)
-if stnmeta_suffix == '.xz':
-    stnmeta_file = lzma.open(args.stnmeta, mode='rt', encoding='utf-8')
-else:
-    stnmeta_file = codecs.open(args.stnmeta, 'r', 'iso-8859-1') # original file is iso-8859
-
-
-# Weather data
-# File: wmo_norms/data/allnorms.dat.utf8.xz
-# Docs: wmo_norms/doc/allnorms.txt
 data = []
 for line in allnorms_file:
     region         = line[0]
@@ -60,6 +82,11 @@ allnorms_file.close()
 # Station metadata
 # File: wmo_norms/data/stnmeta.dat.utf.xz
 # Docs: wmo_norms/doc/stnmeta.txt
+_, stnmeta_suffix  = os.path.splitext(args.stnmeta)
+if stnmeta_suffix == '.xz':
+    stnmeta_file = lzma.open(args.stnmeta, mode='rt', encoding='utf-8')
+else:
+    stnmeta_file = codecs.open(args.stnmeta, 'r', 'iso-8859-1') # original file is iso-8859
 station_meta = []
 for line in stnmeta_file:
     region       = line[0]
@@ -85,187 +112,6 @@ for line in stnmeta_file:
 stnmeta_file.close()
 
 
-# Table 1 of wmo_norms/data/ALLNORMS.TXT
-# Code, Region
-region_code = [
-('01', 'Africa'),
-('02', 'Asia'),
-('03', 'South America'),
-('04', 'North and Central America'),
-('05', 'South-West Pacific'),
-('06', 'Europe'),
-('07', 'Antarctica')
-]
-
-
-# Table 6 of wmo_norms/data/ALLNORMS.TXT
-# Code, Unit, Description
-clim_elem_code = [
-('01', 'deg C',    'Dry Bulb Temperature'),
-('02', 'deg C',    'Maximum Dry Bulb Temperature'),
-('03', 'deg C',    'Minimum Dry Bulb Temperature'),
-('04', 'deg C',    'Wet Bulb Temperature'),
-('05', 'deg C',    'Dew Point Temperature'),
-('06', 'mm',       'Precipitation'),
-('08', 'mm',       'Maximum 24-Hour Precipitation'),
-('09', 'cm',       'Snowfall'),
-('10', 'cm',       'Snow Depth'),
-('11', '%',        'Relative Humidity'),
-('12', 'hPa',      'Sea Level Pressure'),
-('13', 'hPa',      'Station Pressure'),
-('14', 'hPa',      'Vapor Pressure'),
-('15', '*',        'Sunshine'),
-('16', 'm/sec',    'Wind Speed'),
-('17', 'degrees',  'Wind Direction'),
-('18', 'unitless', 'Wind Steadiness'),
-('19', 'deg C',    'Soil Temperature'),
-('20', 'okta',     'Sky Cover (Cloud Cover)'),
-('21', 'mm',       'Pan Evaporation'),
-('28', 'm',        'Height of 1000 hPa Geopotential Level'),
-('29', 'm',        'Height of 850 hPa Geopotential Level'),
-('30', 'm',        'Height of 700 hPa Geopotential level'),
-('32', 'MJ/m2',    'Net Solar Radiation'),
-('33', 'MJ/m2',    'Global Solar Radiation'),
-('34', 'MJ/m2',    'Diffuse Solar Radiation'),
-('35', 'MJ/m2',    'Reflected Solar Radiation'),
-('36', 'MJ/m2',    'Atmospheric Solar Radiation'),
-('37', 'MJ/m2',    'Terrestrial Solar Radiation'),
-('38', 'mm',       'Piche Evaporation'),
-('39', 'mm',       'Rainfall'),
-('40', '*',        'Bright Sunshine'),
-('48', '*',        'Calm Winds'),
-('49', 'count',    'Number Days with Sandstorm/Thick Dust/Haze'),
-('50', 'count',    'Number Days with Measurable Bright Sunshine'),
-('51', 'count',    'Number Days with Thunder'),
-('52', 'count',    'Number Days with Lightning'),
-('53', 'count',    'Number Days with Hail'),
-('54', 'count',    'Number Days with Rainfall GE Threshold'),
-('55', 'count',    'Number Days with Rain Showers'),
-('56', 'count',    'Number Days with Snowfall'),
-('57', 'count',    'Number Days with Snow on Ground'),
-('58', 'count',    'Number Days with Fog/Ice Fog'),
-('59', 'count',    'Number Days with Fog - Sky Obscured'),
-('60', 'count',    'Number Days with Fog - Sky Unobscured'),
-('61', 'count',    'Number Days with Haze/Smoke'),
-('62', 'count',    'Number Days with Dust'),
-('63', 'count',    'Number Days with Blowing Dust/Sand'),
-('65', 'count',    'Number Days with Visibility LE Threshold'),
-('73', 'count',    'Number Days with no Sunshine'),
-('74', 'count',    'Number Days with Dew'),
-('75', 'count',    'Number Days with Rime/Glaze Ice'),
-('76', 'count',    'Number Days with Air Frost'),
-('77', 'count',    'Number Days with Grass Frost'),
-('82', 'count',    'Number Days with Gale Force Winds'),
-('83', 'count',    'Number Days Maximum Temperature GE Threshold'),
-('84', 'count',    'Number Days Maximum Temperature LE Threshold'),
-('85', 'count',    'Number Days Minimum Temperature LE Threshold'),
-('86', 'count',    'Number Days Minimum Temperature GE Threshold'),
-('87', 'count',    'Number Days Mean Temperature GE Threshold'),
-('89', 'count',    'Number Days with Dust/Haze/Mist'),
-('90', 'count',    'Number Days Maximum Temperature GT Threshold'),
-('91', 'count',    'Number Days Maximum Temperature LT Threshold'),
-('92', 'count',    'Number Days Minimum Temperature GT Threshold'),
-('93', 'count',    'Number Days Minimum Temperature LT Threshold'),
-('94', 'count',    'Number Days with Snowfall GE Threshold'),
-('95', 'count',    'Number Days with Precipitation GE Threshold'),
-('96', 'count',    'Number Days with Snow Cover GE Threshold'),
-('97', 'count',    'Number Days with Freezing Rain/Drizzle'),
-('98', 'count',    'Number Days with Blowing Snow'),
-('AA', 'count',    'Number Days with Rain/Drizzle'),
-('AB', 'count',    'Number Days with Snow/Hail'),
-('AC', 'count',    'Number Days with Fog/Mist'),
-('AD', 'count',    'Number Days with Weather Phenomena'),
-('AE', 'count',    'Number Days with Ice Storm'),
-('AF', 'count',    'Number Days with Thick Haze'),
-('AG', 'count',    'Number Days with Rising Sand'),
-('AH', 'count',    'Number Days with Mist'),
-('AI', 'count',    'Number Days with Squalls'),
-('AJ', 'count',    'Number Days with Duststorm/Sandstorm'),
-('AK', 'count',    'Number Days with Sleet/Snow'),
-('BH', 'count',    'Number Days Mean Temperature LT Threshold'),
-('BJ', 'count',    'Number Days with Fog'),
-('BM', 'count',    'Number Days with Daily Maximum Wind Speed GE Threshold'),
-('BT', 'count',    'Number Days with Occurrence of Rain'),
-('BW', 'count',    'Number Days with Daily Maximum Snow Cover GE Threshold')
-]
-
-# Table 7 of wmo_norms/data/ALLNORMS.TXT
-# Code, Description
-statistic_code = [
-('01', 'Mean Value'),
-('02', 'Median Value'),
-('03', 'Standard Deviation of Mean Value'),
-('04', 'Maximum Value'),
-('05', 'Minimum Value'),
-('06', 'Mean Daily Value'),
-('08', 'Standard Deviation of Mean Daily Value'),
-('09', 'Mean Daily Maximum Value'),
-('10', 'Mean Daily Minimum Value'),
-('11', 'Maximum Daily Value'),
-('12', 'Date (Year/Day) of Occurrence of Maximum Daily Value'),
-('13', 'Minimum Daily Value'),
-('14', 'Date (Year/Day) of Occurrence of Minimum Daily Value'),
-('15', 'Mean Monthly Value'),
-('16', 'Standard Deviation of Mean Monthly Value'),
-('18', 'Mean Monthly Maximum Value'),
-('19', 'Mean Monthly Minimum Value'),
-('20', 'Minimum Monthly Value'),
-('21', 'Year of Occurrence of Minimum Monthly Value'),
-('22', 'First Quintile'),
-('23', 'Second Quintile'),
-('24', 'Third Quintile'),
-('25', 'Fourth Quintile'),
-('26', 'Maximum Monthly Value'),
-('27', 'Year of Occurrence of Maximum Monthly Value'),
-('30', 'Maximum Gust '),
-('37', 'Percent of Possible'),
-('38', 'Frequency'),
-('41', 'Prevailing'),
-('42', 'Vector'),
-('44', 'Mean Number of Hours'),
-('45', 'Mean - Sunrise to Sunset'),
-('51', 'Mean on Last Day of Month'),
-('53', 'Percent of Daylight Hours'),
-('55', 'Year of Occurrence of Maximum Value'),
-('56', 'Year of Occurrence of Minimum Value'),
-('57', 'Mean Percent'),
-('58', 'First Quartile'),
-('59', 'Third Quartile'),
-('60', 'Standard Deviation of 3-Hourly Values'),
-('64', 'Total Count for Period of Record'),
-('69', 'Mean of Hourly Observations'),
-('70', 'Mean of Observations at 0000 LST'),
-('71', 'Mean of Observations at 0100 LST'),
-('72', 'Mean of Observations at 0200 LST'),
-('73', 'Mean of Observations at 0300 LST'),
-('74', 'Mean of Observations at 0400 LST'),
-('75', 'Mean of Observations at 0500 LST'),
-('76', 'Mean of Observations at 0600 LST'),
-('77', 'Mean of Observations at 0700 LST'),
-('78', 'Mean of Observations at 0800 LST'),
-('79', 'Mean of Observations at 0900 LST'),
-('80', 'Mean of Observations at 1000 LST'),
-('81', 'Mean of Observations at 1100 LST'),
-('82', 'Mean of Observations at 1200 LST'),
-('83', 'Mean of Observations at 1300 LST'),
-('84', 'Mean of Observations at 1400 LST'),
-('85', 'Mean of Observations at 1500 LST'),
-('86', 'Mean of Observations at 1600 LST'),
-('87', 'Mean of Observations at 1700 LST'),
-('88', 'Mean of Observations at 1800 LST'),
-('89', 'Mean of Observations at 1900 LST'),
-('90', 'Mean of Observations at 2000 LST'),
-('91', 'Mean of Observations at 2100 LST'),
-('92', 'Mean of Observations at 2200 LST'),
-('93', 'Mean of Observations at 2300 LST'),
-('94', 'Mean of 3-Hourly Observations'),
-('97', 'Mean of Synoptic Observations'),
-('98', 'Number of Years used to Calculate Normal'),
-('AF', 'Afternoon Average'),
-('AM', 'Daytime Average'),
-('MO', 'Morning Average'),
-('PM', 'Nighttime Average')
-]
 
 
 
@@ -329,35 +175,35 @@ CREATE TABLE `station_meta` (
     cur.executemany("INSERT INTO `station_meta` VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", station_meta)
 
 
-    cur.execute("DROP TABLE IF EXISTS `region_code`")
+    cur.execute("DROP TABLE IF EXISTS `region_codes`")
     cur.execute("""
-CREATE TABLE `region_code` (
+CREATE TABLE `region_codes` (
     `code`   TEXT PRIMARY KEY,
     `region` TEXT NOT NULL
 );
 """)
-    cur.executemany("INSERT INTO `region_code` VALUES(?,?)", region_code)
+    cur.executemany("INSERT INTO `region_codes` VALUES(?,?)", region_codes)
 
 
-    cur.execute("DROP TABLE IF EXISTS `clim_elem_code`")
+    cur.execute("DROP TABLE IF EXISTS `clim_elem_codes`")
     cur.execute("""
-CREATE TABLE `clim_elem_code` (
+CREATE TABLE `clim_elem_codes` (
     `code`   TEXT PRIMARY KEY,
     `units`  TEXT NOT NULL,
     `desc`   TEXT NOT NULL
 );
 """)
-    cur.executemany("INSERT INTO `clim_elem_code` VALUES(?,?,?)", clim_elem_code)
+    cur.executemany("INSERT INTO `clim_elem_codes` VALUES(?,?,?)", clim_elem_codes)
 
 
-    cur.execute("DROP TABLE IF EXISTS `statistic_code`")
+    cur.execute("DROP TABLE IF EXISTS `statistic_codes`")
     cur.execute("""
-CREATE TABLE `statistic_code` (
+CREATE TABLE `statistic_codes` (
     `code`   TEXT PRIMARY KEY,
     `desc`   TEXT NOT NULL
 );
 """)
-    cur.executemany("INSERT INTO `statistic_code` VALUES(?,?)", statistic_code)
+    cur.executemany("INSERT INTO `statistic_codes` VALUES(?,?)", statistic_codes)
 
 
     cur.execute("VACUUM;")
