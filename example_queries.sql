af99e252c679d4ce49f6f7abfed935f5837f0d10
Jon Dehdari <jon@dehdari.org>
2016-05-31 00:20:30 +0200

    example_queries.sql: cleanup

--- example_queries.sql
+++ example_queries.sql
@@ -1,10 +1,25 @@
 .echo ON
 
 -- Listing of annual sunshine hours in Germany
-select allnorms.station, name, annual_norms_computed from allnorms cross join station_meta where clim_elem_code == '15' and statistic_code == '44' and station_meta.country_name == 'Germany' and allnorms.country == station_meta.country and allnorms.station == station_meta.station order by `annual_norms_computed` desc;
+select allnorms.station, station_meta.name, station_meta.country_name, region_codes.region, annual_norms_computed
+from allnorms
+join station_meta on station_meta.station = allnorms.station
+join region_codes on region_codes.code = allnorms.region
+where clim_elem_code = '15' and statistic_code = '44' and station_meta.country_name = 'Germany'
+order by `annual_norms_computed` desc;
 
 -- Listing of December sunshine hours in Germany
-select allnorms.station, name, allnorms.dec from allnorms cross join station_meta where clim_elem_code == '15' and statistic_code == '44' and station_meta.country_name == 'Germany' and allnorms.country == station_meta.country and allnorms.station == station_meta.station order by allnorms.dec desc;
+select allnorms.station, station_meta.name, station_meta.country_name, region_codes.region, allnorms.dec
+from allnorms
+join station_meta on allnorms.station = station_meta.station
+join region_codes on region_codes.code = allnorms.region
+where clim_elem_code = '15' and statistic_code = '44' and station_meta.country_name = 'Germany'
+order by allnorms.dec desc;
 
 -- Listing of December sunshine hours in US
-select allnorms.station, name, allnorms.dec from allnorms cross join station_meta where clim_elem_code == '15' and statistic_code == '44' and station_meta.country_name == 'United States Of America' and allnorms.country == station_meta.country and allnorms.station == station_meta.station order by allnorms.dec desc  limit 40;
+select allnorms.station, station_meta.name, station_meta.country_name, region_codes.region, allnorms.dec
+from allnorms
+join station_meta on allnorms.station = station_meta.station
+join region_codes on region_codes.code = allnorms.region
+where clim_elem_code = '15' and statistic_code = '44' and station_meta.country_name = 'United States Of America'
+order by allnorms.dec desc  limit 40;
