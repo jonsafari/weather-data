0e03dcccc84adead4ce9a6e7800aadee0491fc0b
Jon Dehdari <jon@dehdari.org>
2016-06-01 01:31:05 +0200

    example_queries.sql++

--- example_queries.sql
+++ example_queries.sql
@@ -11,31 +11,47 @@
 -- order by an_sun.annual_norms_computed desc;
 
 -- Listing of Germany December sunshine hours and mean lows
-select an_sun.station, station_meta.name, station_meta.country_name, region_codes.region, an_sun.dec, an_low.dec
+select an_sun.station,
+	station_meta.name,
+	station_meta.country_name,
+	region_codes.region,
+	an_sun.dec,
+	an_low.dec
 from allnorms an_sun
 join station_meta on an_sun.station = station_meta.station
 join region_codes on region_codes.code = an_sun.region
 join allnorms an_low on an_low.station = an_sun.station
 where station_meta.country_name = 'Germany'
- and an_sun.clim_elem_code = '15' -- sunshine
- and an_sun.statistic_code = '44'
- and an_low.clim_elem_code = '03' -- mean low temp in december
- and an_low.statistic_code = '01'
+	and an_sun.clim_elem_code = '15' -- sunshine
+	and an_sun.statistic_code = '44'
+	and an_low.clim_elem_code = '03' -- mean low temp in december
+	and an_low.statistic_code = '01'
 order by an_sun.dec desc;
 
 
 -- Listing of US December sunshine hours and mean lows
-select an_sun.station, station_meta.name, station_meta.country_name, region_codes.region, an_sun.dec, an_low.dec, an_hi.jul
+select an_sun.station,
+	station_meta.name,
+	station_meta.country_name,
+	region_codes.region,
+	an_sun.dec,
+	an_low.jan,
+	an_hi.jul,
+	an_hi.jul - an_low.jan as "temp_range",
+	an_hum.jul
 from allnorms an_sun
 join station_meta on an_sun.station = station_meta.station
 join region_codes on region_codes.code = an_sun.region
 join allnorms an_low on an_low.station = an_sun.station
-join allnorms an_hi on an_hi.station = an_sun.station
+join allnorms an_hi  on an_hi.station = an_sun.station
+join allnorms an_hum on an_hum.station = an_sun.station
 where station_meta.country_name = 'United States Of America'
- and an_sun.clim_elem_code = '15' -- sunshine
- and an_sun.statistic_code = '44'
- and an_low.clim_elem_code = '03' -- mean low temp in december
- and an_low.statistic_code = '01'
- and an_hi.clim_elem_code = '02' -- mean hi temp in july
- and an_hi.statistic_code = '01'
-order by an_sun.dec desc  limit 40;
+	and an_sun.clim_elem_code = '15' -- sunshine
+	and an_sun.statistic_code = '44'
+	and an_low.clim_elem_code = '03' -- mean low temp (in january)
+	and an_low.statistic_code = '01'
+	and an_hi.clim_elem_code  = '02' -- mean hi temp (in july)
+	and an_hi.statistic_code  = '01'
+	and an_hum.clim_elem_code = '11' -- mean humidity (in july)
+	and an_hum.statistic_code = '94'
+order by an_sun.dec desc  limit 45;
