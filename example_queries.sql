091f6be446c849ec1c576b7f126ce1842b882f23
Jon Dehdari <jon@dehdari.org>
2016-06-01 01:02:51 +0200

    example_queries.sql: +self-joins

--- example_queries.sql
+++ example_queries.sql
@@ -1,25 +1,41 @@
 .echo ON
 
--- Listing of annual sunshine hours in Germany
-select allnorms.station, station_meta.name, station_meta.country_name, region_codes.region, annual_norms_computed
-from allnorms
-join station_meta on station_meta.station = allnorms.station
-join region_codes on region_codes.code = allnorms.region
-where clim_elem_code = '15' and statistic_code = '44' and station_meta.country_name = 'Germany'
-order by `annual_norms_computed` desc;
+-- -- Listing of Germany annual sunshine hours
+-- select an_sun.station, station_meta.name, station_meta.country_name, region_codes.region, an_sun.annual_norms_computed
+-- from allnorms an_sun
+-- join station_meta on station_meta.station = an_sun.station
+-- join region_codes on region_codes.code = an_sun.region
+-- where station_meta.country_name = 'Germany'
+--  and an_sun.clim_elem_code = '15' -- sunshine
+--	and an_sun.statistic_code = '44'
+-- order by an_sun.annual_norms_computed desc;
 
--- Listing of December sunshine hours in Germany
-select allnorms.station, station_meta.name, station_meta.country_name, region_codes.region, allnorms.dec
-from allnorms
-join station_meta on allnorms.station = station_meta.station
-join region_codes on region_codes.code = allnorms.region
-where clim_elem_code = '15' and statistic_code = '44' and station_meta.country_name = 'Germany'
-order by allnorms.dec desc;
+-- Listing of Germany December sunshine hours and mean lows
+select an_sun.station, station_meta.name, station_meta.country_name, region_codes.region, an_sun.dec, an_low.dec
+from allnorms an_sun
+join station_meta on an_sun.station = station_meta.station
+join region_codes on region_codes.code = an_sun.region
+join allnorms an_low on an_low.station = an_sun.station
+where station_meta.country_name = 'Germany'
+ and an_sun.clim_elem_code = '15' -- sunshine
+ and an_sun.statistic_code = '44'
+ and an_low.clim_elem_code = '03' -- mean low temp in december
+ and an_low.statistic_code = '01'
+order by an_sun.dec desc;
 
--- Listing of December sunshine hours in US
-select allnorms.station, station_meta.name, station_meta.country_name, region_codes.region, allnorms.dec
-from allnorms
-join station_meta on allnorms.station = station_meta.station
-join region_codes on region_codes.code = allnorms.region
-where clim_elem_code = '15' and statistic_code = '44' and station_meta.country_name = 'United States Of America'
-order by allnorms.dec desc  limit 40;
+
+-- Listing of US December sunshine hours and mean lows
+select an_sun.station, station_meta.name, station_meta.country_name, region_codes.region, an_sun.dec, an_low.dec, an_hi.jul
+from allnorms an_sun
+join station_meta on an_sun.station = station_meta.station
+join region_codes on region_codes.code = an_sun.region
+join allnorms an_low on an_low.station = an_sun.station
+join allnorms an_hi on an_hi.station = an_sun.station
+where station_meta.country_name = 'United States Of America'
+ and an_sun.clim_elem_code = '15' -- sunshine
+ and an_sun.statistic_code = '44'
+ and an_low.clim_elem_code = '03' -- mean low temp in december
+ and an_low.statistic_code = '01'
+ and an_hi.clim_elem_code = '02' -- mean hi temp in july
+ and an_hi.statistic_code = '01'
+order by an_sun.dec desc  limit 40;
